06b8b76c71a417dd9883b8c78e35851959783f48
Mikail Gökçe <info@groupmkl.com>
2025-12-15 15:32:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9068042525-838d2",
+  "appId": "1:24173500124:web:76fafad2877af3b6e40619",
+  "apiKey": "AIzaSyC4Yf-l73JWIiLFfGQTcC_W4KUIoFmkZuU",
+  "authDomain": "studio-9068042525-838d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "24173500124"
+};
